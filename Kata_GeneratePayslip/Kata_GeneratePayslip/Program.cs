85881327be4e03fdcadc182c753c72fba0ca9ea2
Nick Ye <30612557+nickyemyob@users.noreply.github.com>
2017-08-03 16:30:27 +1200

    variables inputed

--- Kata_GeneratePayslip/Kata_GeneratePayslip/Program.cs
+++ Kata_GeneratePayslip/Kata_GeneratePayslip/Program.cs
@@ -10,6 +10,33 @@ namespace Kata_GeneratePayslip
     {
         static void Main(string[] args)
         {
+            //Input format (first name, last name, annual salary, super rate (%), payment start date)
+            string input = "David,Rudd,60050,9%,01 March – 31 March";
+
+            string firstName, lastName, paymentPeriod, output;
+            int superRate, annualSalary;
+
+            string[] inputData = input.Split(',');
+            firstName = inputData[0];
+            lastName = inputData[1];
+            annualSalary = Int32.Parse(inputData[2]);
+            superRate = inputData[3];
+            paymentPeriod = inputData[4];
+
+            //pay period = per calendar month
+            //gross income = annual salary / 12 months
+            //income tax = based on the tax table provide below
+            //net income = gross income - income tax
+            //super = gross income x super rate
+
+
+
+
+            //Output(name, pay period, gross income, income tax, net income, super)
+            Console.WriteLine(input);
+            //Console.WriteLine(output);
+
+            Console.ReadKey();
         }
     }
 }
